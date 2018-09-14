62dfa6d457cce9021412a976c2ecf279e3da5631
sruthilayadayanandan <42140072+sruthilayadayanandan@users.noreply.github.com>
2018-09-14 18:53:29 +0530

    Add files via upload

---
+++ lab5.5_q1.cpp
@@ -0,0 +1,27 @@
+//include library
+#include<iostream>
+using namespace std;
+
+//using main function
+int main (){
+
+//declaring variables
+int A,a,b;
+
+//asking user for the input
+cout<< " enter the number of series for the pattern; "<<endl;
+cin>>A;
+
+//loop for the number of lines
+for (a=0;a<A;a++)           
+
+//loop for number of stars in a line
+{for (b=0;b<A;b++)
+
+//printing stars
+{cout<<"*";
+}
+cout<<"*";
+cout <<endl;
+}
+}                                 
